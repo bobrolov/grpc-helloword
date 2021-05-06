816b78c838d05adbafbae41e6a9df75de5760635
bobrolov <bobrolov@gmail.com>
2021-05-06 16:26:01 +0300

    write to postgres in implementation

--- src/server.rs
+++ src/server.rs
@@ -7,6 +7,9 @@ use tokio_postgres::{Error, NoTls};
 
 use log::{info, warn};
 
+#[macro_use]
+extern crate derive_new;
+
 use chrono::Utc;
 use postgres::tls::NoTlsStream;
 use postgres::Socket;
@@ -15,8 +18,10 @@ pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
 
-#[derive(Default)]
-pub struct MyGreeter {}
+#[derive(new)]
+pub struct MyGreeter {
+    db_client: tokio_postgres::Client,
+}
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
@@ -32,12 +37,50 @@ impl Greeter for MyGreeter {
 
         let mut reply = hello_world::HelloReply { message: msg };
 
+        &self
+            .write_to_postgres(grpc_message, grpc_client)
+            .await
+            .unwrap();
+        /*
         let res = match write_to_postgres(grpc_message, grpc_client).await {
             Ok(res) => res,
             Err(error) => panic!("Error: {}", error),
         };
-
+        */
         Ok(Response::new(reply))
+    }
+}
+impl MyGreeter {
+    async fn write_to_postgres(&self, grpc_message: &str, grpc_client: &str) -> Result<(), Error> {
+        let db_table = match std::env::var("POSTGRES_TABLE") {
+            Ok(table) => table,
+            Err(error) => panic!("Problem reading the db_table from env: {:?}", error),
+        };
+
+        let datetime = Utc::now().to_rfc3339().replace("T", " ");
+        let datetime = &datetime[..26];
+
+        let db_statement = &self.db_client
+            .prepare(
+                format!(
+                    "INSERT INTO {} (message, client_address, received_at_server) VALUES ($1, $2, $3)",
+                    db_table
+                )
+                    .as_str(),
+            )
+            .await
+            .unwrap();
+        let insert_row = &self
+            .db_client
+            .execute(db_statement, &[&grpc_message, &grpc_client, &datetime])
+            .await;
+        let insert_row = match insert_row {
+            Ok(insert_row) => insert_row,
+            Err(error) => panic!("Problem with insert command: {:?}", error),
+        };
+        info!("Results of insert operation: {}", insert_row);
+
+        Ok(())
     }
 }
 
@@ -59,8 +102,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(error) => panic!("Problem with parsing this address: {:?}", error),
     };
 
-    let greeter = MyGreeter::default();
-
+    let greeter = MyGreeter::new(connect_to_postgres().await.unwrap());
     Server::builder()
         .add_service(GreeterServer::new(greeter))
         .serve(address)
@@ -69,21 +111,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn write_to_postgres(grpc_message: &str, grpc_client: &str) -> Result<(), Error> {
-
+async fn connect_to_postgres() -> Result<tokio_postgres::Client, Error> {
     let config_connection = match std::env::var("POSTGRES_CONFIG") {
         Ok(addr) => addr,
         Err(error) => panic!("Problem reading the config from env: {:?}", error),
     };
     info!("Read config from env: {}", config_connection);
-
-    let db_table = match std::env::var("POSTGRES_TABLE") {
-        Ok(table) => table,
-        Err(error) => panic!("Problem reading the db_table from env: {:?}", error),
-    };
-
-    let datetime = Utc::now().to_rfc3339().replace("T"," ");
-    let datetime = &datetime[..26];
 
     let (client, connection) = tokio_postgres::connect(config_connection.as_str(), NoTls).await?;
 
@@ -93,24 +126,5 @@ async fn write_to_postgres(grpc_message: &str, grpc_client: &str) -> Result<(), 
         }
     });
 
-    let db_statement = client
-        .prepare(
-            format!(
-                "INSERT INTO {} (message, client_address, received_at_server) VALUES ($1, $2, $3)",
-                db_table
-            )
-            .as_str(),
-        )
-        .await
-        .unwrap();
-    let insert_row = client
-        .execute(&db_statement, &[&grpc_message, &grpc_client, &datetime])
-        .await;
-    let insert_row = match insert_row {
-        Ok(insert_row) => insert_row,
-        Err(error) => panic!("Problem with insert command: {:?}", error),
-    };
-    info!("Results of insert operation: {}", insert_row);
-
-    Ok(())
+    Ok(client)
 }
